ac150afb9862f9bb5a9179b994a8843804aa858e
kauebentes@gmail.com <kauebentes@gmail.com>
2021-07-12 23:11:21 -0300

    Primeiro commit

---
+++ src/main/kotlin/com/teste/APIKotlintest/ApiKotlinTestApplication.kt
@@ -0,0 +1,11 @@
+package com.teste.APIKotlintest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiKotlinTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiKotlinTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/teste/APIKotlintest/ApiKotlinTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teste.APIKotlintest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiKotlinTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
